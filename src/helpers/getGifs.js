c833cd67b1810132d250d2251debaecd444b92a5
CamiloPachecoG <58574070+CamiloPachecoG@users.noreply.github.com>
2021-01-01 13:08:32 -0300

    Add new Api Key

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,7 +2,7 @@
 
 export const getGifs = async( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=CLxef7PbAxOBWP44xdM3Mn6TMnsLq616`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=WUHY7FGaa0STgblA79fouShyrw4MpM3p`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
